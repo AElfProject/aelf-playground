574b04fb56d3034515a602063ce5aa02a65a9fa0
yongen.loong <yongen.loong@aelf.io>
2024-05-14 13:10:57 +0800

    chore: fix formatting

--- client/src/tutorials/tutorials.ts
+++ client/src/tutorials/tutorials.ts
@@ -1,13 +1,11 @@
 import { createTutorials } from "./create";
 
 /** All visible tutorials at `/tutorials`(in order) */
-export const TUTORIALS = createTutorials(
-  {
-    name: "Hello Aelf",
-    description: "Hello world program with Native AElf.",
-    authors: [],
-    level: "Beginner",
-    framework: "Native",
-    languages: ["CSharp", "TypeScript"],
-  },
-);
+export const TUTORIALS = createTutorials({
+  name: "Hello Aelf",
+  description: "Hello world program with Native AElf.",
+  authors: [],
+  level: "Beginner",
+  framework: "Native",
+  languages: ["CSharp", "TypeScript"],
+});
