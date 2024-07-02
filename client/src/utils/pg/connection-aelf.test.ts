b01d336ebc03771294a7160bcdaa9d9a1442843b
yongen.loong <yongen.loong@aelf.io>
2024-07-02 16:01:18 +0800

    fix: get transaction details

---
+++ client/src/utils/pg/connection-aelf.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from "vitest";
+import { ConnectionAElf } from "./connection-aelf";
+
+test("should return transaction details", async () => {
+  const aelf = new ConnectionAElf();
+
+  const id = `f500db9f933fb2ca0c27a0835bae0cdcf3342c36369236a313b2d29c9c331aee`;
+  const result = await aelf.getTxResult(id);
+
+  console.log(result);
+  expect(result.TransactionId).toBe(id);
+});
